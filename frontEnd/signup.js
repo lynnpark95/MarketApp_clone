4a919ebe6358371469f5a59e1e3938e0560c1a59
lynnpark95 <lynn.hpark@outlook.com>
2023-10-08 23:05:48 -0600

    ✨feat: password confirm added

--- frontEnd/signup.js
+++ frontEnd/signup.js
@@ -1,5 +1,16 @@
 const form = document.querySelector("#signup-form");
 
+const checkPassword = () => {
+  const formData = new FormData(form);
+  const password = formData.get("password");
+  const pwConfirm = formData.get("pwConfirm");
+  if (password === pwConfirm) {
+    return true;
+  } else {
+    false;
+  }
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
@@ -7,10 +18,24 @@ const handleSubmit = async (event) => {
   formData.set("password", sha256Pw);
   console.log(formData.get("password"));
 
-  const res = await fetch("/signup", {
-    method: "POST",
-    body: formData,
-  });
+  const div = document.querySelector("#pwCheck");
+
+  if (checkPassword()) {
+    const res = await fetch("/signup", {
+      method: "POST",
+      body: formData,
+    });
+
+    //only display success message when you get 200 message
+    const data = await res.json();
+    if (data === "200") {
+      div.innerText = "Thanks for signing up♥";
+      div.style.color = "blue";
+    }
+  } else {
+    div.innerText = "Passwords are not matching.";
+    div.style.color = "red";
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
